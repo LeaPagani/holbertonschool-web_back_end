3097281144a9c6ad1c66ce554239b72212ff05ea
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-04-23 11:22:14 -0400

    task 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""takes list of floats and ints and returns their sum as a float"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """return sum of list floats & ints"""
+    return sum(mxd_lst)
