11358b14ccada289aa4a7a5f34c6f061781232c0
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-05-17 23:17:15 -0400

    lint fix for tasks 1-4

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,4 +1,4 @@
 function signUpUser(firstName, lastName) {
-    return Promise.resolve({ firstName, lastName });
-  }
-  export default signUpUser;
+  return Promise.resolve({ firstName, lastName });
+}
+export default signUpUser;
