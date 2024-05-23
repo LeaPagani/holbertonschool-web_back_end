9ddd4041daac3c0d87b4b73dec88ba0490ccdf40
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-05-23 08:48:04 -0400

    eslint for task 0

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,9 @@
 function getListStudents() {
-    const obj = [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
-      ]
+  const obj = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }];
+  return obj;
+
 }
+
