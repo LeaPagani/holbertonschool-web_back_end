429e9339ce6d3b5245d622142d6b16dcdfc03cf1
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-05-23 08:46:36 -0400

    task 0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+function getListStudents() {
+    const obj = [
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+        { id: 5, firstName: 'Serena', location: 'San Francisco' }
+      ]
+}
